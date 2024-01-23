1dee8fae41411415522892283baee7a43daa832b
nc <31476486+nirc7@users.noreply.github.com>
2024-01-23 12:20:44 +0200

    Update CCChild.jsx

--- Lec03 23_01_2024 CC List/cc-list-demo/src/ClassComps/CCChild.jsx
+++ Lec03 23_01_2024 CC List/cc-list-demo/src/ClassComps/CCChild.jsx
@@ -6,6 +6,10 @@ export default class CCChild extends Component {
     super(props);
 
     this.list = ["avi ", "benny", "charlie"];
+
+    this.state = {
+      persons: [{ id: 1, name: 'avi' }, { id: 2, name: 'benny' }]
+    };
   }
 
   componentDidMount() {
@@ -16,14 +20,29 @@ export default class CCChild extends Component {
     console.log('unmount');
   }
 
+  AddPerson = () => {
+    let newPReonsList = [...this.state.persons, {id:3, name:'charlie'}];
+    console.log(newPReonsList);
+    this.setState({persons : newPReonsList});
+  }
+
   render() {
 
     this.listAsLi = this.list.map((name, index) =>
       <a href="#" className="list-group-item" key={index}>{index + ": hello " + name + "!"}</a>);
 
+    let personsStr = this.state.persons.map((per) =>
+      <a href="#" className="list-group-item" key={per.id}>id={per.id} -- name={per.name}</a>);
+
+
     console.log('render in cchld');
     return (
       <>
+        <p style={{ fontWeight: "bold", margin: 10 }}>persons list:</p>
+        <div className="list-group" style={{ width: "70%" }}>{personsStr}</div>
+
+        <button onClick={this.AddPerson}>Add Person</button>
+
         <p style={{ fontWeight: "bold", margin: 10 }}>names list:</p>
         <div className="list-group" style={{ width: "70%" }}>{this.listAsLi}</div>
 
